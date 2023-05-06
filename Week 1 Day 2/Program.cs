d4ba21f1640ed6be1ca64bea19bff642b057b104
Kevin Soo <000411471@student.vcc.ca>
2023-05-06 11:17:02 -0700

    Week 1 Day 2

---
+++ Week 1 Day 2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-templusing System;
+
+namespace _1303Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Enter one or more lines - press CTRL Z to exit");
+            string line;
+
+            do
+            {
+                Console.Write("===>");
+                line = Console.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine($"{line}");
+                }
+            } while (line != null);
+        }
+    }
+}
