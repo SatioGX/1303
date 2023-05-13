73a81369bd821a2f5689806d86c693364f39da45
SatioGX <78477842+SatioGX@users.noreply.github.com>
2023-05-12 19:45:44 -0700

    Lab 3

---
+++ Labs/Lab 3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class User
+{
+    public int ID;
+    public string Name;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User[] users = new User[10];
+
+        for (int i = 0; i < users.Length; i++)
+        {
+            users[i] = new User();
+            users[i].ID = i + 1;
+            users[i].Name = "placeholder" + (i + 1);
+        }
+
+        PrintUserArray(users);
+    }
+
+    static void PrintUserArray(User[] users)
+    {
+        for (int i = 0; i < users.Length; i++)
+        {
+            Console.WriteLine("User ID: " + users[i].ID + ", Name: " + users[i].Name);
+        }
+    }
+}
