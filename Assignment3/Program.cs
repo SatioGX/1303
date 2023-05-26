4ffbf8645cce0736794abd2e8882b49428d61db1
SatioGX <78477842+SatioGX@users.noreply.github.com>
2023-05-26 04:02:05 -0700

    Assignment 3

---
+++ Assignment3/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        myCar.brand = "Toyota";
+        myCar.year = 2020;
+        myCar.numWheels = 4;
+        myCar.NumDoors = 4;
+        myCar.Drive();
+
+        Truck myTruck = new Truck();
+        myTruck.brand = "Ford";
+        myTruck.year = 2018;
+        myTruck.numWheels = 6;
+        myTruck.LoadCapacity = 5000;
+        myTruck.Drive();
+
+        Bus myBus = new Bus();
+        myBus.brand = "Mercedes";
+        myBus.year = 2015;
+        myBus.numWheels = 8;
+        myBus.NumSeats = 40;
+        myBus.Drive();
+
+        Motorcycle myMotorcycle = new Motorcycle();
+        myMotorcycle.brand = "Harley-Davidson";
+        myMotorcycle.year = 2021;
+        myMotorcycle.numWheels = 2;
+        myMotorcycle.HasSidecar = false;
+        myMotorcycle.Drive();
+    }
+}
---
+++ Assignment3/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿// Base Vehicle class
+public class Vehicle
+{
+    public string brand { get; set; }
+    public int year { get; set; }
+    public int numWheels { get; set; }
+
+    public virtual void Drive()
+    {
+        Console.WriteLine($"Driving a {brand} with {numWheels} wheels");
+    }
+}
+
+// Car class derived from Vehicle
+public class Car : Vehicle
+{
+    public int NumDoors { get; set; }
+
+}
+
+// Truck class derived from Vehicle
+public class Truck : Vehicle
+{
+    public int LoadCapacity { get; set; }
+
+}
+
+// Bus class derived from Vehicle
+public class Bus : Vehicle
+{
+    public int NumSeats { get; set; }
+
+}
+
+// Motorcycle class derived from Vehicle
+public class Motorcycle : Vehicle
+{
+    public bool HasSidecar { get; set; }
+
+}
