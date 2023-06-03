f2c4c9701c0ae661f9ae69e6c7a6c371581a4b4e
000411471 <000411471@CC147XX1A007XXX>
2023-06-03 10:28:52 -0700

    Lab 4

--- Lab 4/MyStack.cs
+++ Lab 4/MyStack.cs
@@ -3,28 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//public class MyStack
-//{
-//    public MyStack()
-//    {
-//    }
-
-//    private const int Size = 5;
-//    private int position;
-
-//    private object[] items = new object[Size];
-
-//    public void Push(object obj)
-//    {
-//        items[position++] = obj;
-
-//    }
-
-//    public object Pop()
-//    {
-//        return items[--position];
-//    }
-//}
 public class MyStack
 {
     private int size;
