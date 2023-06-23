3178a27c364f06f939aa68b16dab65cc0173efb3
SatioGX <78477842+SatioGX@users.noreply.github.com>
2023-06-23 01:54:24 -0700

    Assignment 7

---
+++ Assignment7/LinkedList.cs
@@ -0,0 +1,136 @@
+﻿namespace Assignment7
+{
+    /// <summary>
+    /// Singly Linked List
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LinkedList<T>
+    {
+        private int count;
+
+        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> Tail { get; private set; }
+
+        /// <summary>
+        /// Add a node to the beginning of the list
+        /// </summary>
+        /// <param name="node"></param>
+        public void AddHead(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = this.Head;
+
+            this.Head = node;
+
+            this.Head.Next = temp;
+            // this.count++;
+        }
+
+        /// <summary>
+        /// Add a node to the end of the list
+        /// </summary>
+        /// <param name="node"></param>
+        public void AddTail(LinkedListNode<T> node) 
+        { 
+            if (this.count == 0)
+            {
+                this.Head = node;
+            }
+            else
+            {
+                this.Tail.Next = node;
+            }
+
+            this.Tail = node;
+            this.count++;
+        }
+
+        /// <summary>
+        /// Remove the node located at the head
+        /// Before: Head --> 3 --> 5
+        /// After:  Head --> 5
+        /// </summary>
+        public void RemoveHead()
+        {
+            this.Head = this.Head.Next;
+            this.count--;
+
+            if (this.count == 0)
+            {
+                this.Tail = null;
+            }
+        }
+
+        /// <summary>
+        /// Remove the node located at the tail
+        /// </summary>
+        public void RemoveTail()
+        {
+            if (this.count == 0)
+            {
+                // List is empty, nothing to remove
+                return;
+            }
+            else if (this.count == 1)
+            {
+                // List has only one node, remove it
+                this.Head = null;
+                this.Tail = null;
+                this.count = 0;
+                return;
+            }
+            else
+            {
+                // Traverse the list to find the node before the tail
+                LinkedListNode<T> current = this.Head;
+                while (current.Next != this.Tail)
+                {
+                    current = current.Next;
+                }
+
+                // Update tail and remove the current tail node
+                current.Next = null;
+                this.Tail = current;
+                this.count--;
+            }
+        }
+
+        public bool Remove(T data)
+        {
+            LinkedListNode<T> current = this.Head;
+            LinkedListNode<T> previous = null;
+            
+            while(current != null)
+            { 
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                        {
+                            this.Tail = previous;
+                        }
+
+                        this.count--;
+                    }
+                    else
+                    {
+                        this.RemoveHead();
+                    }
+
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public int GetCount()
+        {
+            return this.count;
+        }
+    }
+}
---
+++ Assignment7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Assignment7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // var test = new DelegateExample();
+            // test.TestDelegateFunctionality();
+
+            var test = new Program();
+            //test.AddNodeToHead_ShouldAdd();
+
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddTail(new LinkedListNode<int>(2));
+            linkedList.AddTail(new LinkedListNode<int>(4));
+            linkedList.AddTail(new LinkedListNode<int>(6));
+
+            Console.WriteLine("Before removing tail:");
+            Console.WriteLine("Count: " + linkedList.GetCount());
+            Console.WriteLine("Head: " + linkedList.Head.Data);
+            Console.WriteLine("Tail: " + linkedList.Tail.Data);
+
+            linkedList.RemoveTail();
+
+            Console.WriteLine("After removing tail:");
+            Console.WriteLine("Count: " + linkedList.GetCount());
+            Console.WriteLine("Head: " + linkedList.Head.Data);
+            Console.WriteLine("Tail: " + linkedList.Tail.Data);
+        }
+
+        //private void AddNodeToHead_ShouldAdd()
+        //{
+        //    var linkedList = new LinkedList<int>();
+
+        //    linkedList.AddHead(new LinkedListNode<int>(2));
+
+        //    var count = linkedList.GetCount();
+        //    if (count != 1)
+        //    {
+        //        throw new Exception("Count should be 1");
+        //    }
+        //}
+
+
+
+    }
+
+}
